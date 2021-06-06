f00e2dfa57981061ded5c46bdba5457f5c662c18
Vajith Chamuditha <vajithc@gmail.com>
2021-06-06 23:46:53 +0530

    Modified

--- untitled/src/HouseRobber.java
+++ untitled/src/HouseRobber.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class HouseRobber {
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter no: of houses:- ");
