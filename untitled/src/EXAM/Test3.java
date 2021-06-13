d34c0425efbbf88f36cd77f117c9cac1af7695a5
Vajith Chamuditha <vajithc@gmail.com>
2021-06-13 21:56:37 +0530

    Modified

---
+++ untitled/src/EXAM/Test3.java
@@ -0,0 +1,10 @@
+package EXAM;
+
+public class Test3 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder=new StringBuilder("i love java");
+        //stringBuilder.substring(0,9);
+        stringBuilder.reverse();
+        System.out.println(stringBuilder);
+    }
+}
---
+++ untitled/src/EXAM/Test4.java
@@ -0,0 +1,11 @@
+package EXAM;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int i=5;
+        while (i<10){
+            System.out.println(i++ + "h");
+            System.out.println(++i);
+        }
+    }
+}
